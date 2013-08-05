0be35d6ca6fa672e3ea08ada24f3e26c8c9342d0
Kevin F Browne <kbrowneatx@gmail.com>
2013-08-05 13:32:01 -0500

    implemented new command flow, home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,31 +24,9 @@ function add_fields(link, association, content) {
     var new_id = new Date().getTime();
     var regexp = new RegExp("new_" + association, "g");
     $(link).parent().before(content.replace(regexp, new_id));
-    $('#new-battalion-fields').modal('show');
 };
 
 $(document).ready(function() {
-
-	$('#myCarousel').carousel({
-			interval: 5000
-		});
- 
-		$('#carousel-text').html($('#slide-content-0').html());
- 
-		//Handles the carousel thumbnails
-	$('[id^=carousel-selector-]').click( function(){
-		var id_selector = $(this).attr("id");
-		var id = id_selector.substr(id_selector.length -1);
-		var id = parseInt(id);
-		$('#myCarousel').carousel(id);
-		});
- 
- 
-		// When the carousel slides, auto update the text
-	$('#myCarousel').on('slid', function (e) {
-		var id = $('.item.active').data('slide-number');
-		$('#carousel-text').html($('#slide-content-'+id).html());
-	});
 		
   $('#army_era_id').change(function() {
     $.ajax({
@@ -60,15 +38,12 @@ $(document).ready(function() {
     });
   });
 
-    $(document).on("click", ".open-SetUnitClass", function () {
-        var myBattalionId = $(this).data('id');
-        $(".modal-body #btlnId").val( myBattalionId );
-        // As pointed out in comments,
-        // it is superfluous to have to manually call the modal.
-        // $('#addBookDialog').modal('show');
-    });
+	$(document).on("click", ".open-SetUnitClass", function () {
+			var myBattalionId = $(this).data('id');
+			$(".modal-body #btlnId").val( myBattalionId );
+			// As pointed out in comments,
+			// it is superfluous to have to manually call the modal.
+			// $('#addBookDialog').modal('show');
+	});
 		
-});
-
-
 });
